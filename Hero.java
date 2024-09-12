ca8c19bacf68f57e7ec1bb06e9f33b45ac206d0b
MaggieJiang7 <147207333+MaggieJiang7@users.noreply.github.com>
2024-09-12 12:39:53 -0400

    Add Hero

--- Hero.java
+++ Hero.java
@@ -8,12 +8,18 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Hero extends Actor
 {
-    /**
-     * Act - do whatever the Hero wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    boolean atTop = true;
     public void act()
     {
-        if (Greenfoot.mouseClicked(
+        if (Greenfoot.mouseClicked(null))
+        {
+            atTop = !atTop;
+        }
+        
+        if (atTop){
+            setLocation(100,100);
+        } else {
+            setLocation (100, 300);
+        }
     }
 }

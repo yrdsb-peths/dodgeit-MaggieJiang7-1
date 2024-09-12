349921147f3b3bfd8a676b6bd5ab75059d5d60a2
MaggieJiang7 <147207333+MaggieJiang7@users.noreply.github.com>
2024-09-12 12:46:02 -0400

    Reset Banana

--- Banana.java
+++ Banana.java
@@ -15,5 +15,21 @@ public class Banana extends Actor
     public void act()
     {
         move(-3);
+        
+        if (getX() <=0)
+        {
+            resetBanana();
+        }
+    }
+    
+    public void resetBanana()
+    {
+        int num = Greenfoot.getRandomNumber(2);
+        if (num == 0)
+        {
+            setLocation (600,100);
+        } else {
+            setLocation (600, 300);
+        }
     }
 }
